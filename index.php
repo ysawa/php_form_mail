e231d9100dceb98c38a6b3e23b154d150bd1bb9d
Yoshihiro Sawa <yoshihirosawa@gmail.com>
2013-02-10 22:45:27 +0900

    add: first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php require_once './default.php'; ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang='ja'>
+<head>
+<?php include 'head.php'; ?>
+</head>
+<body>
+<h1>
+Hello, world!
+</h1>
+</body>
+</html>
