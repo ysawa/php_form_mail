2bd164665ec6d8673454a1a3509b043818ad0d41
Yoshihiro Sawa <yoshihirosawa@gmail.com>
2013-02-12 17:33:43 +0900

    modify: send_mail command into finish.php

--- finish.php
+++ finish.php
@@ -1,4 +1,15 @@
 <?php require_once './default.php'; ?>
+<?php
+
+$mailto = "y_sawa@atoj.co.jp";
+$subject = "タイトル";
+$content = "内容";
+$mail_headers = "From:" . mb_encode_mimeheader("澤 良弘") . "<yoshihirosawa@gmail.com>";
+
+$additional_parameters="-f yoshihirosawa@gmail.com";
+
+mb_send_mail($mailto, $subject, $content, $mail_headers, $additional_parameters);
+?>
 <?php $subtitle = ''; ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html lang='ja'>
